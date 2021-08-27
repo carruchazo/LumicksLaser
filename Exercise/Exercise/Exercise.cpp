0fd9dc451d237a0620758866d8573c619376d5a5
carruchazo <juanljt@hotmail.com>
2021-08-27 23:20:37 +0200

    Implemented main loop

--- Exercise/Exercise/Exercise.cpp
+++ Exercise/Exercise/Exercise.cpp
@@ -2,18 +2,25 @@
 //
 
 #include <iostream>
+#include <string>
+#include "CLaserController.h"
+#include "CCommandInterpreter.h"
+
+using namespace std;
+using namespace NS_Laser_Controller;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    CLaserController controller;
+    CCommandInterpreter interpreter;
+    interpreter.SetPointerToLaserController(&controller);
+
+    // runs indefinitely
+    while (true)
+    {
+        string next;
+        cin >> next;
+        cout << interpreter.ParseCommand(next);
+    }
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
